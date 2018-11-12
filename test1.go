5462b76e9f7968cfc8b9a715cf693f17873060eb
cc <c@c.com>
2018-11-12 13:55:42 -0500

    test

---
+++ test1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myInt int
+	myInt = 42
+	println(myInt)
+	var myFloat32 float32=42.
+	println(myFloat32)
+	myString := "Hello Go"
+	println(myString)
+   myArray := [...]int{42, 27, 99}
+   fmt.Println(myArray)
+}
+
